418d7bd4c1b1d96293016d521a5db499f22b40aa
jonahng <nyhj1998@gmail.com>
2024-11-14 09:45:42 +0800

    created spring template

---
+++ src/main/java/com/jonah/vttp5a_day11l/Vttp5aDay11lApplication.java
@@ -0,0 +1,13 @@
+package com.jonah.vttp5a_day11l;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5aDay11lApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5aDay11lApplication.class, args);
+	}
+
+}
