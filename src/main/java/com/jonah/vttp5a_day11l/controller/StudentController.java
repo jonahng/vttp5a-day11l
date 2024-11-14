2ffe10f1bf97707241e52e08ff00f45102a56e26
jonahng <nyhj1998@gmail.com>
2024-11-14 15:52:34 +0800

    created and retrieved student class list

--- src/main/java/com/jonah/vttp5a_day11l/controller/StudentController.java
+++ src/main/java/com/jonah/vttp5a_day11l/controller/StudentController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.jonah.vttp5a_day11l.model.Student;
 
@@ -23,16 +24,24 @@ public class StudentController {
         students = new ArrayList<>();
     }
 
+    
+    @ResponseBody
     //GetMapping("/allStudents")
     @RequestMapping(path = {"","/allStudents"},method=RequestMethod.GET)
     public List<Student> getStudentList() throws ParseException{
 
-        String dob = "18 Dec 1999 10:2500.000 SGT";
+        String dob = "18 Dec 1999 10:25:00.000 SGT";
         SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy HH:mm:ss.SSS zzz");
         Date dDob = sdf.parse(dob); //DATE from javautil
         Long epocDob = dDob.getTime();
         
-        Student sl = new Student(1, "Dave", "tan", epocDob, "dave@google.com", "29 road");
+        Student s1 = new Student(1, "Dave", "tan", epocDob, "dave@google.com", "29 road");
+        students.add(s1);
+
+        //to handle date, use Long and String
+        //UI Controller
+        //Rest Controller for Data
+        //use ResponseBody
 
         return students;
     }
